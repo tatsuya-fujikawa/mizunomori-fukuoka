ba5b2ab897c90e74e6409b4d4a2f36051d0721be
fujikawa <kyuji123456@yahoo.co.jp>
2022-04-19 13:16:05 +0900

    ハンバーガーメニュー制作

--- src/js/humberger.js
+++ src/js/humberger.js
@@ -1,6 +1,8 @@
 export function humbergerButton() {
     const button = document.querySelector('.js-humberger');
+    const nav = document.getElementById('js-nav')
     button.addEventListener('click', () => {
         button.classList.toggle('is-open');
+        nav.classList.toggle('is-open');
     });
 };
